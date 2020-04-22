2c1f991dde9f76f43a047c6c5e7a9f31dca1fec6
17739511536 <1181426017@qq.com>
2020-04-22 19:24:22 +0800

    update

--- produce.py
+++ produce.py
@@ -3,6 +3,7 @@ import json
 import time
 import datetime
 
+
 from kafka import KafkaProducer
 
 
@@ -10,13 +11,22 @@ class TestKafka:
     def __init__(self):
         self.producer = KafkaProducer(bootstrap_servers=config.SERVER,
                                       value_serializer=lambda m: json.dumps(m).encode())
+        print(self.producer)
+
+    def on_send_success(self, record_metadata):
+        print(record_metadata.topic)
+        print(record_metadata.partition)
+        print(record_metadata.offset)
+
+    def on_send_error(self, excp):
+        print('I am an errback')
+        print(excp)
 
     def start(self):
         for i in range(1, 1000):
             data = {'num': i, 'ts': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')}
-            print(data)
-            self.producer.send(config.TOPIC, data)
-            time.sleep(0.1)
+            self.producer.send(config.TOPIC, data).add_callback(self.on_send_success).add_errback(self.on_send_error)
+            # time.sleep(0.1)
 
     def main(self):
         self.start()
